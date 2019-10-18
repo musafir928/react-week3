61839017a4ac663a515ebb5a97b4deeedd88ddb4
musafir928 <uganoghli@gmail.com>
2019-10-19 00:44:23 +0200

    react3

---
+++ build/precache-manifest.91ed37d3f6ea08154ee586fb7dcd422f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74800c4de540cfcb0563862451e7b7a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2279872b549d5108bae0",
+    "url": "/static/css/main.aa4a1872.chunk.css"
+  },
+  {
+    "revision": "3ec0a2a60f7fcc5b2e38",
+    "url": "/static/js/2.d47ded65.chunk.js"
+  },
+  {
+    "revision": "2279872b549d5108bae0",
+    "url": "/static/js/main.76b275fa.chunk.js"
+  },
+  {
+    "revision": "722362a7198e7580534e",
+    "url": "/static/js/runtime-main.3cdc0666.js"
+  }
+]);
